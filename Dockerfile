3250f12875fc41d613e6bf906342a1e70f3594a0
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-09-11 12:44:28 +0000

    chore: Preparing to add api subrepo

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-# Builds a Docker to deliver dist/
-FROM garlictech2/angular2-app-server:v2.1.24
-COPY dist/ /usr/share/nginx/html
--- src/subrepos/api-ngx/store/index.ts
+++
@@ -1,9 +0,0 @@
-import * as Actions from './actions';
-
-export type Action = Actions.AllActions;
-export { Effects } from './effects';
-
-import { IAuthenticationState } from './state';
-export { IAuthenticationState, IAuth, domain } from './state';
-export { reducer as Reducer } from './reducer';
-export { Actions };
