d811e90890f5ea66ec22c373e75b9444b885b428
Petró Tamás <petro.tamas@gmail.com>
2017-09-24 20:19:58 +0200

    log format fix

--- src/app/log.ts
+++ src/app/log.ts
@@ -2,7 +2,7 @@ import { Log, Level } from 'ng2-logger/ng2-logger'
 import { Logger } from 'ng2-logger/src/logger'
 
 export const log = Log.create('@pioneer-wst/main-website')
-log.color = "black"
+log.color = 'black'
 
 export function DebugLog(target: Object, key: string, descriptor: TypedPropertyDescriptor<any>) {
   return {
