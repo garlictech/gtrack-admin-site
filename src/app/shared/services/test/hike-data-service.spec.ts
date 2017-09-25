9e4907ac238df0973c3dc51cac7c965288a8d122
Petró Tamás <petro.tamas@gmail.com>
2017-09-25 15:59:02 +0200

    initial unit test file for services

---
+++ src/app/shared/services/test/hike-data-service.spec.ts
@@ -0,0 +1,10 @@
+import { HikeDataService } from '../hike-data.service';
+import { MOCK_HIKE_LIST, MOCK_HIKE_DATA } from '../../../mock-data';
+
+describe('HikeDataService', () => {
+    let service: HikeDataService;
+
+    beforeEach(() => {
+        service = new HikeDataService();
+    });
+});
