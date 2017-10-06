5e17bbe5c496f6eda9005a6140df405195210122
Petró Tamás <petro.tamas@gmail.com>
2017-10-07 01:38:41 +0200

    Set login page title

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { State } from '../../../store';
 import { Actions } from 'authentication-api-ngx';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'gt-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-  constructor(private _store: Store<State>) {}
+export class LoginComponent implements OnInit {
+  constructor(
+    private _store: Store<State>,
+    private _title: Title
+  ) {}
+
+  ngOnInit() {
+    this._title.setTitle('Login');
+  };
 
   public login() {
     this._store.dispatch(new Actions.GoogleLogin(['admin']));
