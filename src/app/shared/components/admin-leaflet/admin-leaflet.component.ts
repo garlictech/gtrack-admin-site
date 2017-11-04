67062cfaecb286df22cc8ebf442f20fa72af0fed
Petró Tamás <petro.tamas@gmail.com>
2017-11-04 01:04:39 +0100

    feat: save map by id in adminMapService

--- src/app/shared/components/admin-leaflet/admin-leaflet.component.ts
+++ src/app/shared/components/admin-leaflet/admin-leaflet.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 import { AdminMap, AdminMapService } from '../../services/admin-map';
 import { LeafletComponent } from '../../../../subrepos/gtrack-common-ngx/app';
 
@@ -7,6 +7,8 @@ import { LeafletComponent } from '../../../../subrepos/gtrack-common-ngx/app';
   template: '<div #map class="angular-leaflet-map"><ng-content></ng-content></div>'
 })
 export class AdminLeafletComponent extends LeafletComponent implements AfterViewInit {
+  @Input()
+  public id = 'admin-map';
   public map: AdminMap;
 
   constructor(private adminMapService: AdminMapService) {
--- src/app/shared/services/admin-map/admin-map.service.ts
+++ src/app/shared/services/admin-map/admin-map.service.ts
@@ -1,5 +1,5 @@
 import { Store } from '@ngrx/store';
-import { State } from '../../../store';
+import { State, Actions } from '../../../store';
 import { Injectable } from '@angular/core';
 import { AdminMap } from './admin-map';
 import {
@@ -13,26 +13,37 @@ import {
 
 @Injectable()
 export class AdminMapService extends MapService {
+  protected _maps: {[id: string]: AdminMap} = {};
+
   constructor(
     protected iconService: IconService,
     protected mapMarkerService: MapMarkerService,
     private _store: Store<State>,
     private _gameRuleService: GameRuleService,
     private _routeService: RouteService,
-    private _elevationService: ElevationService
+    private _elevationService: ElevationService,
+    private _actions: Actions
   ) {
     super(iconService, mapMarkerService);
   }
 
-  public get(map: L.Map): AdminMap {
-    return new AdminMap(
-      map,
+  public get(leafletMap: L.Map, id: string): AdminMap {
+    const map = new AdminMap(
+      leafletMap,
       this.iconService,
       this.mapMarkerService,
       this._store,
       this._gameRuleService,
       this._routeService,
-      this._elevationService
+      this._elevationService,
+      this._actions
     );
+    this._maps[id] = map;
+
+    return map;
+  }
+
+  getMapById(id: string) {
+    return this._maps[id];
   }
 }
