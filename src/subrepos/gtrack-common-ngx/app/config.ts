7551459069cd43bd5aa369ca466702fe1f6a9042
Petró Tamás <petro.tamas@gmail.com>
2017-11-25 00:54:38 +0100

    fix: places library to google map config

--- src/subrepos/gtrack-common-ngx/app/config.ts
+++ src/subrepos/gtrack-common-ngx/app/config.ts
@@ -1,10 +1,12 @@
 export interface IGoogleMapsConfig {
   key: string;
+  libraries?: string[];
   v?: string;
 };
 
 export class CommonConfig {
   googleMaps: IGoogleMapsConfig = {
-    key: 'AIzaSyANByCixyD2mLtE80aUooldhc3E9W1NqGQ'
+    key: 'AIzaSyANByCixyD2mLtE80aUooldhc3E9W1NqGQ',
+    libraries: ['geometry', 'places']
   };
 }
