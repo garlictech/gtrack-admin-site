8d6ad6e9ca0602484ff8a086044a2e76b1d3c8e1
Petró Tamás <petro.tamas@gmail.com>
2017-11-25 20:40:03 +0100

    fix: update IPoi field definitions for IExternalPoi compatibility

--- src/subrepos/gtrack-common-ngx/app/hike/services/poi/ipoi.ts
+++ src/subrepos/gtrack-common-ngx/app/hike/services/poi/ipoi.ts
@@ -1,14 +1,14 @@
 export interface IPoi {
-  id: string;
-  elevation: string;
+  id?: string;
+  elevation?: string;
   lat: number;
   lon: number;
-  objectType: string;
-  title: string;
-  types: string[];
-  segment: any;
-  inHike: boolean;
-  distanceFromOrigo: number;
-  isStart: boolean;
-  isFinish: boolean;
+  objectType?: string;
+  title?: string;
+  types?: string[];
+  segment?: any;
+  inHike?: boolean;
+  distanceFromOrigo?: number;
+  isStart?: boolean;
+  isFinish?: boolean;
 }
