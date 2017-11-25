849c9339cbdbd9b5bb1de6371dd8e239cc3261e2
Petró Tamás <petro.tamas@gmail.com>
2017-11-25 18:40:38 +0100

    feat: geometry service methods

--- src/subrepos/gtrack-common-ngx/app/hike/services/geometry/index.ts
+++ src/subrepos/gtrack-common-ngx/app/hike/services/geometry/index.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as turf from '@turf/turf';
 
 export type GeoPoint = {
   lat: number;
@@ -23,5 +24,21 @@ export class GeometryService {
 
   public toRadian(n: number): number {
     return (n * Math.PI) / 180;
+  }
+
+  public distanceFromRoute(lonLat, path) {
+    let point = turf.point(lonLat);
+    let p2 = turf.pointOnLine(path, point);
+    return Math.round(1000 * turf.distance(point, p2, 'kilometers'));
+  }
+
+  public getCenterRadius(bounds) {
+    const p1 = this.getPoint(bounds.SouthWest.lon, bounds.SouthWest.lat);
+    const p2 = this.getPoint(bounds.NorthEast.lon, bounds.NorthEast.lat);
+
+    return {
+      radius: turf.distance(p1, p2, 'kilometers') * 500,
+      center: turf.midpoint(p1, p2)
+    }
   }
 }
