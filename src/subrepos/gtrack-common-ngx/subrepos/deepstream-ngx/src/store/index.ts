742b5feb135f622b071a32248b5c436fbfceaf24
Petró Tamás <petro.tamas@gmail.com>
2017-11-25 12:26:51 +0100

    remove subrepos folder before re-clone

--- src/subrepos/gtrack-common-ngx/subrepos/deepstream-ngx/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-// export * from './selectors';
-import * as Actions from './actions';
-
-export type Action = Actions.AllActions;
-export { Effects } from './effects';
-
-export { IDeepstreamState } from './state';
-export { reducer as Reducer } from './reducer';
--- src/subrepos/gtrack-common-ngx/subrepos/deepstream-ngx/src/store/state.ts
+++
@@ -1,8 +0,0 @@
-import { IClientData } from '@garlictech/deepstream-rxjs';
-
-export interface IDeepstreamState {
-  state: string;
-  failure: any;
-  auth: IClientData;
-  permissionRecord: any;
-}
