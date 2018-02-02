be5f44554694ad277ac36ed7bb23cc8db361e73c
Petró Tamás <petro.tamas@gmail.com>
2018-02-02 11:58:52 +0100

    git subrepo clone https://github.com/garlictech/deepstream-ngx.git src/subrepos/deepstream-ngx
    subrepo:
      subdir:   "src/subrepos/deepstream-ngx"
      merged:   "d5cee5e"
    upstream:
      origin:   "https://github.com/garlictech/deepstream-ngx.git"
      branch:   "master"
      commit:   "d5cee5e"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ src/subrepos/deepstream-ngx/src/store/effects.ts
@@ -0,0 +1,46 @@
+import { Injectable, Inject } from '@angular/core';
+import { Actions, Effect, toPayload } from '@ngrx/effects';
+import { Action, Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Rx';
+
+import { IClientData } from '@garlictech/deepstream-rxjs';
+
+import { log } from '../log';
+// import { Actions as JwtApiActions, IAuth } from 'authentication-api-ngx';
+import { DeepstreamService } from '../deepstream-service';
+import * as LocalActions from './actions';
+import { EXTERNAL_DEEPSTREAM_DEPENDENCIES, IExternalDeepstreamDependencies } from '../lib/externals';
+
+interface IAuth {
+  token: string;
+}
+
+@Injectable()
+export class Effects {
+  @Effect()
+  deepstreamLogin$: Observable<Action> = this._actions$
+    .ofType(LocalActions.DEEPSTREAM_LOGIN)
+    .map(toPayload)
+    .switchMap((token: string) => {
+      log.d('Effect: Logging in to deepstream...');
+      return this._deepstreamService.login(token);
+    })
+    .filter(auth => !!auth)
+    .map((auth: IClientData) => {
+      log.i('Effect: Deepstream auth success. Auth objct: ', auth);
+      return new LocalActions.DeepstreamAuthSuccess(auth);
+    })
+    .catch(err => {
+      log.er('Effect: Deepstream auth error', err);
+      return Observable.of(new LocalActions.DeepstreamLoginFailed(err));
+    });
+
+  constructor(
+    private _actions$: Actions,
+    private _deepstreamService: DeepstreamService,
+    private _store: Store<any>,
+    @Inject(EXTERNAL_DEEPSTREAM_DEPENDENCIES) private _externals: IExternalDeepstreamDependencies
+  ) {
+    /* EMPTY */
+  }
+}
