f073ca073888514210cebef846bff5ecaf370062
Petró Tamás <petro.tamas@gmail.com>
2018-02-02 11:58:09 +0100

    chore: remove subrepos before re-clone

--- src/subrepos/gtrack-common-ngx/app/hike/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './components';
-export * from './hike.module';
-export * from './store';
--- src/subrepos/gtrack-common-ngx/app/hike/services/hike-program/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './hike-program';
-export * from './hike-program.service';
-export * from './interfaces';
