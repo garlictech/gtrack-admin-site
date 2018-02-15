016eb0263fee073c5506432d41dd7986590a835f
Petró Tamás <petro.tamas@gmail.com>
2018-02-15 22:14:38 +0100

    chore: clear subrepos before re-clone

--- src/subrepos/gtrack-common-ngx/app/hike/services/segment/isegment.ts
+++
@@ -1,8 +0,0 @@
-export interface ISegment {
-  distance: number;
-  uphill: number;
-  downhill: number;
-  coordinates: number[][];
-  time?: number;
-  score?: number;
-}
--- src/subrepos/provider-client/README.md
+++
@@ -1,3 +0,0 @@
-# Client library for gtrack provider users
-
-Mainly interfaces and validators shared between the client and the server.
