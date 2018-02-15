29f17d239570c61a0a33eea44516a6819d04b1f8
Petró Tamás <petro.tamas@gmail.com>
2018-02-15 22:15:28 +0100

    git subrepo clone https://github.com/garlictech/authentication-api-ngx.git src/subrepos/authentication-api-ngx
    subrepo:
      subdir:   "src/subrepos/authentication-api-ngx"
      merged:   "af8cd96"
    upstream:
      origin:   "https://github.com/garlictech/authentication-api-ngx.git"
      branch:   "master"
      commit:   "af8cd96"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ src/subrepos/authentication-api-ngx/api/api.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Http, Headers, Response } from '@angular/http';
+import { LocalStorage } from '../storage/local-storage.service';
+import { Observable } from 'rxjs/Observable';
+import * as Actions from '../store/actions';
+
+@Injectable()
+export class ApiService {
+  constructor(
+    private _http: Http,
+    private _localStorage: LocalStorage,
+    private _store: Store<any>
+  ) {}
+
+  get(url: string): Observable<Response> {
+    let headers = this.getAuthorizationHeaders();
+
+    return this._http
+      .get(url, {
+        headers: headers
+      })
+      .catch(err => this.handleError(err));
+  }
+
+  post(url: string, data: any): Observable<Response> {
+    let headers = this.getAuthorizationHeaders();
+
+    return this._http
+      .post(url, data, {
+        headers: headers
+      })
+      .catch(err => this.handleError(err));
+  }
+
+  private getAuthorizationHeaders(): Headers {
+    let token = this._localStorage.getItem('token');
+    let headers = new Headers();
+
+    headers.append('Authorization', 'JWT ' + token);
+
+    return headers;
+  }
+
+  private handleError(err: Response | Error): Observable<Response> {
+    if (err instanceof Response) {
+      if (err.status === 401) {
+        this._store.dispatch(new Actions.Unauthorized());
+      }
+    }
+
+    return Observable.throw(err);
+  }
+}
