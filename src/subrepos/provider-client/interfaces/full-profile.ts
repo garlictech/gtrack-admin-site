1d8db336a72b962b48b752defe5d716a1c2f3fbf
Petró Tamás <petro.tamas@gmail.com>
2018-03-22 22:51:29 +0100

    git subrepo clone https://github.com/garlictech/gtrack-provider-client.git src/subrepos/provider-client
    subrepo:
      subdir:   "src/subrepos/provider-client"
      merged:   "ce76760"
    upstream:
      origin:   "https://github.com/garlictech/gtrack-provider-client.git"
      branch:   "master"
      commit:   "ce76760"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ src/subrepos/provider-client/interfaces/full-profile.ts
@@ -0,0 +1,9 @@
+import { IUserData, IPublicUserProfile } from './user';
+
+export interface IFullUserData {
+  user?: IUserData;
+}
+
+export interface IPublicProfile {
+  user?: IPublicUserProfile;
+}
