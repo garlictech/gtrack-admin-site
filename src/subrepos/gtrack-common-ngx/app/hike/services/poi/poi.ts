ab73070cefb6e3977a155a4871013db27164e9b1
Petró Tamás <petro.tamas@gmail.com>
2018-03-22 22:18:59 +0100

    feat: store updates for editing poi

--- src/subrepos/gtrack-common-ngx/app/hike/services/poi/poi.ts
+++ src/subrepos/gtrack-common-ngx/app/hike/services/poi/poi.ts
@@ -1,16 +1,19 @@
 import { IPoi, EPoiTypes, ILocalizedItem, ITextualDescription } from 'subrepos/provider-client';
+import * as _ from 'lodash';
 
 export class Poi implements IPoi {
-  public id?: string;
+  public id: string;
   public elevation: number;
   public lat: number;
   public lon: number;
   public objectType: EPoiTypes;
   public types: string[] = [];
   public description: ILocalizedItem<ITextualDescription>;
+  public tags: string[] = [];
 
   public constructor(data: IPoi) {
-    Object.assign(this, data);
+    // Temporary fix for copying isCheckpoint getter
+    _.assign(this, _.omit(data, ['isCheckpoint']));
   }
 
   public setToCheckpoint() {
