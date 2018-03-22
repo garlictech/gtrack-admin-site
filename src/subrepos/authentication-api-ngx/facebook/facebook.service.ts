c6a470999ac7f35044726830ffabc40dc2cfbb40
Petró Tamás <petro.tamas@gmail.com>
2018-03-22 23:29:37 +0100

    git subrepo clone https://github.com/garlictech/authentication-api-ngx.git src/subrepos/authentication-api-ngx
    subrepo:
      subdir:   "src/subrepos/authentication-api-ngx"
      merged:   "747ba13"
    upstream:
      origin:   "https://github.com/garlictech/authentication-api-ngx.git"
      branch:   "master"
      commit:   "747ba13"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ src/subrepos/authentication-api-ngx/facebook/facebook.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { OauthWindowService } from '../oauth-window';
+import { AuthService } from '../auth';
+import { WindowService } from '../window';
+import { IAuth } from '../store';
+import { AuthenticationApiConfig, IFacebookConfig } from '../lib/config';
+import { DebugLog } from '../log';
+import { AuthProviderBase } from '../auth-provider-base';
+
+@Injectable()
+export class FacebookService extends AuthProviderBase {
+  private facebookApiUrl: string;
+  private config: IFacebookConfig;
+
+  constructor(
+    private authConfig: AuthenticationApiConfig,
+    auth: AuthService,
+    oauthWindow: OauthWindowService,
+    private windowService: WindowService,
+    http: Http
+  ) {
+    super(oauthWindow, http, auth);
+    this._init();
+  }
+
+  /**
+   * Do the complete facebook authentication process
+   */
+  @DebugLog
+  public connect(roles?: string[]): Promise<IAuth> {
+    let redirectUri = this.config.redirectUri || `${this.authConfig.webserverUrl}/facebook/success.html`;
+
+    return this._connect(
+      `${this.facebookApiUrl}/dialog/oauth?client_id=${this.config.appId}`,
+      redirectUri,
+      `${this.authConfig.apiUrl}/auth/facebook/token`,
+      this.config.permissions,
+      roles
+    );
+  }
+
+  @DebugLog
+  private _init() {
+    this.config = this.authConfig.facebook;
+
+    let window: any = this.windowService.nativeWindow;
+    this.facebookApiUrl = `https://www.facebook.com/v${this.config.version}`;
+
+    window.facebookOauthCallback = (url: string) => {
+      this.oauthWindow.close();
+      this._oauthCallback(url);
+    };
+  }
+}
---
+++ src/subrepos/authentication-api-ngx/store/effects.ts
@@ -0,0 +1,176 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
+import { Observable } from 'rxjs/Rx';
+
+import { AuthService } from '../auth';
+import { TwitterService } from '../twitter';
+import { FacebookService } from '../facebook';
+import { GoogleService } from '../google';
+import { PasswordlessService } from '../passwordless';
+import { log } from '../log';
+import * as LocalActions from './actions';
+import { LoginError, LoginErrorCodes } from '../errors/login-error';
+
+@Injectable()
+export class Effects {
+  // Twitter login
+  @Effect()
+  twitterLogin$: Observable<Action> = this._actions$
+    .ofType<LocalActions.TwitterLogin>(LocalActions.TWITTER_LOGIN)
+    .switchMap(action => {
+      log.d('Effect: Twitter login initiated');
+      return Observable.fromPromise(this._twitter.connect(action.roles))
+        .map(auth => {
+          log.i('Effect: Twitter login success');
+          return new LocalActions.LoginSuccess(auth);
+        })
+        .catch(err => {
+          log.er('Effect: Twitter Login error', err);
+          return Observable.of(new LocalActions.FailureHappened(err));
+        });
+    });
+
+  // Twitter token verification
+  @Effect()
+  requestVerifyToken$: Observable<Action> = this._actions$
+    .ofType<LocalActions.RequestVerifyToken>(LocalActions.REQUEST_VERIFY_TOKEN)
+    .switchMap(action => {
+      log.d('Effect: Requesting Twitter email verification token');
+      return Observable.fromPromise(this._auth.requestVerifyToken(action.email))
+        .map(auth => {
+          log.i('Effect: Twitter verification email sent');
+          return new LocalActions.MagicLinkEmailSent();
+        })
+        .catch(err => {
+          log.er('Effect: Twitter Login error', err);
+          return Observable.of(new LocalActions.FailureHappened(err));
+        });
+    });
+
+  // Verify
+  @Effect()
+  verify$: Observable<Action> = this._actions$
+    .ofType<LocalActions.Verify>(LocalActions.VERIFY)
+    .switchMap(action => {
+      log.d('Effect: Verify initiated');
+      return Observable.fromPromise(this._auth.verify(action.token, action.uid))
+        .map(auth => {
+          log.i('Effect: Verify success');
+          return new LocalActions.LoginSuccess(auth);
+        })
+        .catch(err => {
+          log.er('Effect: Verify error', err);
+          return Observable.of(new LocalActions.FailureHappened(err));
+        });
+  });
+
+  // Google login
+  @Effect()
+  googleLogin$: Observable<Action> = this._actions$
+    .ofType<LocalActions.GoogleLogin>(LocalActions.GOOGLE_LOGIN)
+    .switchMap(action => {
+      log.d('Effect: Google login initiated');
+      return Observable.fromPromise(this._google.connect(action.roles))
+        .map(auth => {
+          log.i('Effect: Google login success');
+          return new LocalActions.LoginSuccess(auth);
+        })
+        .catch(err => {
+          log.er('Effect: Google Login error', err);
+          return Observable.of(new LocalActions.FailureHappened(err));
+        });
+    });
+
+  // Facebook login
+  @Effect()
+  facebookLogin$: Observable<Action> = this._actions$
+    .ofType<LocalActions.FacebookLogin>(LocalActions.FACEBOOK_LOGIN)
+    .switchMap(action => {
+      log.d('Effect: Facebook login initiated');
+      return Observable.fromPromise(this._facebook.connect(action.roles))
+        .map(auth => {
+          log.i('Effect: Facebook login success');
+          return new LocalActions.LoginSuccess(auth);
+        })
+        .catch(err => {
+          log.er('Effect: Facebook Login error', err);
+          return Observable.of(new LocalActions.FailureHappened(err));
+        });
+    });
+
+  // Magic Link login
+  @Effect()
+  magiclinkRequestToken$: Observable<Action> = this._actions$
+    .ofType<LocalActions.RequestMagicLinkToken>(LocalActions.MAGICLINK_REQUEST_TOKEN)
+    .switchMap(action => {
+      log.d('Effect: Magic Link login initiated');
+      return this._magicLink
+        .requestToken(action.email, action.language, action.roles)
+        .map(() => {
+          log.i('Effect: Magic Link email sent');
+          return new LocalActions.MagicLinkEmailSent();
+        })
+        .catch(err => {
+          log.er('Effect: Magic Link email sending', err);
+          return Observable.of(new LocalActions.FailureHappened(err));
+        });
+    });
+
+  // Magic Link login
+  @Effect()
+  magiclinkLogin$: Observable<Action> = this._actions$
+    .ofType<LocalActions.MagicLinkLogin>(LocalActions.MAGICLINK_LOGIN)
+    .switchMap(action => {
+      log.d('Effect: Magic Link login initiated');
+      return Observable.fromPromise(this._magicLink.callback(action.token, action.uid, action.roles))
+        .map(auth => {
+          log.i('Effect: Magic Link login success');
+          return new LocalActions.LoginSuccess(auth);
+        })
+        .catch(err => {
+          log.er('Effect: Magic Link Login error', err);
+          return Observable.of(new LocalActions.FailureHappened(err));
+        });
+    });
+
+  // Logout
+  @Effect()
+  logout$: Observable<Action> = this._actions$
+    .ofType<LocalActions.LogoutStart>(LocalActions.LOGOUT_START)
+    .switchMap(() => {
+      log.d('Effect: Logout initiated');
+      return Observable.fromPromise(this._auth.logout())
+        .map(() => {
+          log.i('Effect: Logout success');
+          return new LocalActions.LogoutSuccess();
+        })
+        .catch(err => {
+          log.er('Effect: Logout error', err);
+          return Observable.of(new LocalActions.FailureHappened(err));
+        });
+    });
+
+  @Effect()
+  unauthorized$: Observable<Action> = this._actions$
+    .ofType<LocalActions.Unauthorized>(LocalActions.UNAUTHORIZED)
+    .switchMap(() => {
+      return Observable.fromPromise(this._auth.refreshToken())
+        .map(auth => {
+          return new LocalActions.LoginSuccess(auth);
+        })
+        .catch(err => {
+          log.er('Effect: Refresh token error', err);
+          return Observable.of(new LocalActions.LogoutStart());
+        });
+    });
+
+  constructor(
+    private _actions$: Actions,
+    private _twitter: TwitterService,
+    private _google: GoogleService,
+    private _facebook: FacebookService,
+    private _magicLink: PasswordlessService,
+    private _auth: AuthService
+  ) { }
+}
