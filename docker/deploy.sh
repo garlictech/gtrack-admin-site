5c9b1f451b74b0aa3eb9311d1075db431c40fdbe
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-03-25 21:43:54 +0200

    chore: deployment script fix

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+TARGET=staging
+
+if [[ $TRAVIS_BRANCH == "master" ]]; then
+  TARGET=master
+fi
+
 docker run -i -t --rm \
   -v $(pwd):/app/project \
   -e FIREBASE_TOKEN="$FIREBASE_TOKEN" \
-  garlictech2/workflows-common:${npm_package_config_dockerWorkflowVersion} scripts/firebase-deploy.sh
+  garlictech2/workflows-common:${npm_package_config_dockerWorkflowVersion} scripts/firebase-deploy.sh $TARGET
