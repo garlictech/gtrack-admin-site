462001bed6807741cbd1661446c851696aa8c3e5
Petró Tamás <petro.tamas@gmail.com>
2018-03-31 20:24:57 +0200

    fix: auth modula and auth import fix

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
+import { AuthenticationSelectors } from 'subrepos/gtrack-common-ngx';
+import { DeepstreamActions } from 'subrepos/gtrack-common-ngx/app/deepstream';
+import { Store } from '@ngrx/store';
+import { State } from '../store';
 
 const COMPONENTS = [
   LoginComponent
@@ -12,4 +16,19 @@ const COMPONENTS = [
   declarations: [...COMPONENTS],
   entryComponents: [...COMPONENTS]
 })
-export class AuthModule {}
+export class AuthModule {
+  constructor(
+    private _authSelectors: AuthenticationSelectors.Selectors,
+    private _store: Store<State>
+  ) {
+    this._init();
+  }
+
+  private _init() {
+    this._store
+      .select(this._authSelectors.jwtLoggingIn)
+      .filter(loggingIn => !loggingIn)
+      .switchMapTo(this._store.select(this._authSelectors.token))
+      .subscribe(token => this._store.dispatch(new DeepstreamActions.DeepstreamLogin(token)));
+  }
+}
