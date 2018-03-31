e0a48fc621e9a3d5b0f2e11b689ba70a2f566794
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-03-31 14:52:21 +0200

    git subrepo pull (merge) src/subrepos/gtrack-common-ngx
    subrepo:
      subdir:   "src/subrepos/gtrack-common-ngx"
      merged:   "f0d3690"
    upstream:
      origin:   "https://github.com/garlictech/gtrack-common-ngx.git"
      branch:   "master"
      commit:   "f0d3690"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a356961"

--- src/subrepos/gtrack-common-ngx/app/hike/store/route/test/effects.spec.ts
+++ src/subrepos/gtrack-common-ngx/app/hike/store/route/test/effects.spec.ts
@@ -1,143 +1,146 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
-import { Actions, Effect, EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { DeepstreamService } from 'subrepos/deepstream-ngx';
+// import { HttpClientTestingModule } from '@angular/common/http/testing';
+// import { TestBed } from '@angular/core/testing';
+// import { Actions, Effect, EffectsModule } from '@ngrx/effects';
+// import { StoreModule } from '@ngrx/store';
+// import { DeepstreamService } from 'subrepos/deepstream-ngx';
 
-import { Observable } from 'rxjs/Observable';
+// import { Observable } from 'rxjs/Observable';
 
-import * as _ from 'lodash';
-import * as uuid from 'uuid/v4';
+// import * as _ from 'lodash';
+// import * as uuid from 'uuid/v4';
 
-import { hot, cold } from 'jasmine-marbles';
+// import { hot, cold } from 'jasmine-marbles';
 
-import { IRouteStored, IRoute } from 'subrepos/provider-client';
-import { Route, RouteService } from '../../../services/route';
-import { RouteEffects } from '../effects';
-import * as routeActions from '../actions';
-import { DeepstreamModule } from '../../../../deepstream';
-import { UnitsService } from '../../../../shared';
+// import { IRouteStored, IRoute } from 'subrepos/provider-client';
+// import { Route, RouteService } from '../../../services/route';
+// import { RouteEffects } from '../effects';
+// import * as routeActions from '../actions';
+// import { DeepstreamModule } from '../../../../deepstream';
+// import { UnitsService } from '../../../../shared';
 
-class TestActions extends Actions {
-  constructor() {
-    super(Observable.empty());
-  }
+// class TestActions extends Actions {
+//   constructor() {
+//     super(Observable.empty());
+//   }
 
-  set stream(source: Observable<any>) {
-    this.source = source;
-  }
-}
+//   set stream(source: Observable<any>) {
+//     this.source = source;
+//   }
+// }
 
-export function getActions() {
-  return new TestActions();
-}
+// export function getActions() {
+//   return new TestActions();
+// }
 
-describe('Route effects', () => {
-  let routeData: IRoute;
-  let routeDataStored: IRouteStored;
-  let route: Route;
-  let actions$: TestActions;
-  let routeService: RouteService;
-  let effects: RouteEffects;
+// describe('Route effects', () => {
+//   let routeData: IRoute;
+//   let routeDataStored: IRouteStored;
+//   let route: Route;
+//   let actions$: TestActions;
+//   let routeService: RouteService;
+//   let effects: RouteEffects;
 
-  let id: string;
-  let newId: string;
+//   let id: string;
+//   let newId: string;
 
-  beforeEach(() => {
-    id = uuid();
-    newId = uuid();
+//   beforeEach(() => {
+//     id = uuid();
+//     newId = uuid();
 
-    routeData = {
-      bounds: {
-        NorthEast: {
-          lat: 47.498993,
-          lon: 19.043699
-        },
-        SouthWest: {
-          lat: 47.497157,
-          lon: 19.049757
-        }
-      },
-      route: {
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            properties: {
-              name: 'Test'
-            },
-            geometry: {
-              type: 'Point',
-              coordinates: [18.95623999999998, 47.57855, 305.3887023925781]
-            }
-          }
-        ]
-      }
-    };
+//     routeData = {
+//       bounds: {
+//         NorthEast: {
+//           lat: 47.498993,
+//           lon: 19.043699
+//         },
+//         SouthWest: {
+//           lat: 47.497157,
+//           lon: 19.049757
+//         }
+//       },
+//       route: {
+//         type: 'FeatureCollection',
+//         features: [
+//           {
+//             type: 'Feature',
+//             properties: {
+//               name: 'Test'
+//             },
+//             geometry: {
+//               type: 'Point',
+//               coordinates: [18.95623999999998, 47.57855, 305.3887023925781]
+//             }
+//           }
+//         ]
+//       }
+//     };
 
-    routeDataStored = {
-      ...routeData,
-      id,
-      timestamp: new Date().getTime()
-    };
+//     routeDataStored = {
+//       ...routeData,
+//       id,
+//       timestamp: new Date().getTime()
+//     };
 
-    route = new Route(routeDataStored);
+//     route = new Route(routeDataStored);
 
-    TestBed.configureTestingModule({
-      imports: [
-        StoreModule.forRoot({}),
-        EffectsModule.forRoot([]),
-        HttpClientTestingModule,
-        DeepstreamModule.forRoot()
-      ],
-      providers: [
-        RouteService,
-        RouteEffects,
-        UnitsService,
-        {
-          provide: Actions,
-          useFactory: getActions
-        },
-        {
-          provide: DeepstreamService,
-          useValue: {}
-        }
-      ]
-    });
+//     TestBed.configureTestingModule({
+//       imports: [
+//         StoreModule.forRoot({}),
+//         EffectsModule.forRoot([]),
+//         HttpClientTestingModule,
+//         DeepstreamModule.forRoot({
+//           storeDomain: 'deepstream',
+//           deepstreamConnectionString: ''
+//         })
+//       ],
+//       providers: [
+//         RouteService,
+//         RouteEffects,
+//         UnitsService,
+//         {
+//           provide: Actions,
+//           useFactory: getActions
+//         },
+//         {
+//           provide: DeepstreamService,
+//           useValue: {}
+//         }
+//       ]
+//     });
 
-    actions$ = TestBed.get(Actions);
-    routeService = TestBed.get(RouteService);
-    effects = TestBed.get(RouteEffects);
+//     actions$ = TestBed.get(Actions);
+//     routeService = TestBed.get(RouteService);
+//     effects = TestBed.get(RouteEffects);
 
-    spyOn(routeService, 'get').and.callFake(_id => Observable.of(new Route(routeDataStored)));
-    spyOn(routeService, 'create').and.returnValue(
-      Observable.of({
-        id: newId
-      })
-    );
-  });
+//     spyOn(routeService, 'get').and.callFake(_id => Observable.of(new Route(routeDataStored)));
+//     spyOn(routeService, 'create').and.returnValue(
+//       Observable.of({
+//         id: newId
+//       })
+//     );
+//   });
 
-  describe('loadRoute$', () => {
-    it('should return a Route from RouteLoaded', () => {
-      const action = new routeActions.LoadRoute(id);
-      const completion = new routeActions.RouteLoaded(id, route);
-      const expected = cold('-b', { b: completion });
+//   describe('loadRoute$', () => {
+//     it('should return a Route from RouteLoaded', () => {
+//       const action = new routeActions.LoadRoute(id);
+//       const completion = new routeActions.RouteLoaded(id, route);
+//       const expected = cold('-b', { b: completion });
 
-      actions$.stream = hot('-a', { a: action });
+//       actions$.stream = hot('-a', { a: action });
 
-      expect(effects.loadRoute$).toBeObservable(expected);
-    });
-  });
+//       expect(effects.loadRoute$).toBeObservable(expected);
+//     });
+//   });
 
-  describe('saveRoute$', () => {
-    it('should return the id of the created Route from RouteCreated', () => {
-      const action = new routeActions.SaveRoute(routeData);
-      const completion = new routeActions.RouteSaved(newId);
-      const expected = cold('-b', { b: completion });
+//   describe('saveRoute$', () => {
+//     it('should return the id of the created Route from RouteCreated', () => {
+//       const action = new routeActions.SaveRoute(routeData);
+//       const completion = new routeActions.RouteSaved(newId);
+//       const expected = cold('-b', { b: completion });
 
-      actions$.stream = hot('-a', { a: action });
+//       actions$.stream = hot('-a', { a: action });
 
-      expect(effects.saveRoute$).toBeObservable(expected);
-    });
-  });
-});
+//       expect(effects.saveRoute$).toBeObservable(expected);
+//     });
+//   });
+// });
