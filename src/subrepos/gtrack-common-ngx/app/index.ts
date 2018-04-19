d5b70f88a53f9ef5e98aefc1f29fb66b7561ceef
Petró Tamás <petro.tamas@gmail.com>
2018-04-19 14:24:12 +0200

    chore: package update

--- src/subrepos/gtrack-common-ngx/app/index.ts
+++ src/subrepos/gtrack-common-ngx/app/index.ts
@@ -8,10 +8,6 @@ export * from './router';
 export * from './styles';
 export * from './store';
 export * from './geosearch';
-<<<<<<< HEAD
-export * from './shared/services/background-geolocation-service';
-export * from './search-filters';
-=======
+
 export * from './user-status';
 export * from './utils';
->>>>>>> fix: adding multi language component
