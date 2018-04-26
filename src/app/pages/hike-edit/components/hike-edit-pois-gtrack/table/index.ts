c0f76072988e1e33bc02c08712208238d58b2c6e
Petró Tamás <petro.tamas@gmail.com>
2018-04-26 20:20:02 +0200

    refactor: geospatialService moved to common-ngx

--- src/app/pages/hike-edit/components/hike-edit-pois-gtrack/table/index.ts
+++ src/app/pages/hike-edit/components/hike-edit-pois-gtrack/table/index.ts
@@ -5,6 +5,8 @@ import { State, hikeEditPoiActions, editedHikeProgramActions } from 'app/store';
 import { IGTrackPoi } from 'app/shared/interfaces';
 import { LanguageService } from 'app/shared/services';
 import { IDynamicComponentModalConfig, DynamicModalService } from 'subrepos/gtrack-common-ngx';
+import { HikeEditRoutePlannerSelectors } from '../../../../../store/selectors';
+import { GeospatialService } from 'subrepos/gtrack-common-ngx/app/shared/services/geospatial';
 
 @Component({
   selector: 'hike-edit-pois-gtrack-table',
@@ -16,14 +18,41 @@ export class HikeEditPoisGTrackTableComponent {
 
   constructor(
     private _store: Store<State>,
-    private _dynamicModalService: DynamicModalService
+    private _dynamicModalService: DynamicModalService,
+    private _geospatialService: GeospatialService,
+    private _hikeEditRoutePlannerSelectors: HikeEditRoutePlannerSelectors,
   ) {}
 
-  public addPoi($event, poi) {
+  public addPoi($event, poi: IGTrackPoi) {
     $event.stopPropagation();
 
-    this._store.dispatch(new editedHikeProgramActions.AddPoi(poi.id));
+    this._store
+      .select(this._hikeEditRoutePlannerSelectors.getRoute)
+      .take(1)
+      .subscribe((route) => {
+        const stop = {
+          distanceFromOrigo: this._geospatialService.distanceOnLine(
+            [route.features[1].geometry.coordinates[0], route.features[1].geometry.coordinates[1]],
+            [poi.lon, poi.lat],
+            route.features[0]
+          ),
+          onRoute: poi.onRoute || false,
+          poiId: poi.id,
+          lat: poi.lat,
+          lon: poi.lon,
+          segment: { // TODO: w/ turf
+            uphill: 0,
+            downhill: 0,
+            distance: 0,
+            score: 0,
+            time: 0
+          }
+        }
 
+        this._store.dispatch(new editedHikeProgramActions.AddStop(stop));
+      });
+
+    // TODO: remove
     this._store.dispatch(new hikeEditPoiActions.SetListDirty({
       subdomain: 'gTrack',
       dirty: true
