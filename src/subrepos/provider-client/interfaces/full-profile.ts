3163510c2533ccb6018983ebc594454616d41c25
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-04-27 23:00:46 +0200

    n/a

--- src/subrepos/provider-client/interfaces/full-profile.ts
+++
@@ -1,9 +0,0 @@
-import { IUserData, IPublicUserProfile } from './user';
-
-export interface IFullUserData {
-  user?: IUserData;
-}
-
-export interface IPublicProfile {
-  user?: IPublicUserProfile;
-}
