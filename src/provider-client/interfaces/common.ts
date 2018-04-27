9b31479e658cc96729cc571082853ef10bcf370f
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-04-27 23:01:10 +0200

    git subrepo clone https://github.com/garlictech/gtrack-provider-client.git src/provider-client
    subrepo:
      subdir:   "src/provider-client"
      merged:   "dcac00c"
    upstream:
      origin:   "https://github.com/garlictech/gtrack-provider-client.git"
      branch:   "master"
      commit:   "dcac00c"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ src/provider-client/interfaces/common.ts
@@ -0,0 +1,9 @@
+export interface ITextualDescription {
+  title: string,
+  summary?: string,
+  fullDescription?: string
+}
+
+export interface ILocalizedItem<T> {
+  [s: string]: T
+}
---
+++ src/provider-client/interfaces/poi.ts
@@ -0,0 +1,43 @@
+import { IProviderInput } from './provider';
+import { ILocalizedItem, ITextualDescription } from './common';
+
+export enum EPoiTypes {
+  google = 'google',
+  wikipedia = 'wikipedia',
+  osmAmenity = 'osmAmenity',
+  osmNatural = 'osmNatural',
+  osmRoute = 'osmRoute'
+}
+
+export interface IPoiArchiveInput extends IProviderInput {
+  id: string;
+}
+
+export interface IPoiSaveResponse {
+  success: boolean;
+  id: string;
+}
+
+export interface IPoi {
+  id?: string;
+  elevation: number;
+  lat: number;
+  lon: number;
+  objectType?: EPoiTypes;
+  types: string[];
+  description: ILocalizedItem<ITextualDescription>;
+  tags?: string[];
+  objectId?: {
+    [objectType: string]: string | ILocalizedItem<string>; // Google/OSM pageid, wiki multi ids
+  };
+  additionalData?: {
+    [key: string]: string; // url/address/phoneNumber/openingHours
+  };
+}
+
+export interface IPoiInput extends IPoi, IProviderInput {}
+
+export interface IPoiStored extends IPoi {
+  id: string;
+  timestamp: number;
+}
