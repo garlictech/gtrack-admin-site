e7b2b48f35aa7a8876e37eff40a967b5b90a3bad
Petró Tamás <petro.tamas@gmail.com>
2018-06-14 15:00:43 +0200

    remove forms-ngx before re-clone

--- src/subrepos/forms-ngx/field/emoji.ts
+++
@@ -1,9 +0,0 @@
-import { TextareaField, ITextareaField } from './textarea';
-
-export class EmojiField extends TextareaField {
-  controlType = 'emoji';
-
-  constructor(options: ITextareaField) {
-    super(options);
-  }
-}
