3f0e8b6cfab66fdbbbe2a270e935afd775517fc0
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-06-16 01:34:17 +0200

    fix: workflow + aot upgrade

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -3,8 +3,31 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { ToasterModule } from 'angular2-toaster';
 import { EffectsModule } from '@ngrx/effects';
+import * as _ from 'lodash';
+import { environment } from 'environments/environment';
 
-import { GtrackCommonModule, DynamicModalModule } from 'subrepos/gtrack-common-ngx';
+import {
+  GtrackCommonModule,
+  DynamicModalModule,
+  SearchFiltersModule,
+  DeepstreamModule,
+  AuthenticationModule as CommonAuthenticationModule,
+  SharedModule,
+  HikeEffects,
+  RouteEffects,
+  PoiEffects,
+  GeoSearchModule,
+  GeoSearchEffects,
+  defaultSharedConfig,
+  IHikeModuleConfig,
+  BackgroundGeolocationModule,
+  BackgroundGeolocationEffects,
+  HikeModule
+} from 'subrepos/gtrack-common-ngx';
+import { DeepstreamModule as CommonDeepstreamModule } from 'subrepos/deepstream-ngx';
+
+import { defaultAuthenticationApiConfig, AuthenticationApiModule } from 'subrepos/authentication-api-ngx';
+
 import { REDUCER_TOKEN, metaReducers } from './store';
 import { LanguageModule } from './language';
 import { CoreLayoutModule } from './core';
@@ -19,17 +42,59 @@ import {
   HikeEditPoiEffects,
   AuthEffects
 } from './store/effects';
+import { GenericComponentsModule } from './generic-components';
+
+const sharedConfig = { ...defaultSharedConfig };
+
+const hikeModuleConfig: IHikeModuleConfig = {
+  storeDomains: {
+    hike: 'hike',
+    poi: 'poi',
+    route: 'route'
+  }
+};
+
+const config = {
+  ...defaultAuthenticationApiConfig
+};
+
+config.apiUrl = environment.authentication.server;
+config.webserverUrl = environment.webappServer;
+config.facebook.appId = _.get(environment, 'authentication.facebook.appId');
+config.google.appId = _.get(environment, 'authentication.google.appId');
+config.magiclink = { redirectSlug: '/auth/magiclink' };
+
+export function getConfig() {
+  return config;
+}
 
 export const APP_IMPORTS = [
   StoreModule.forRoot(REDUCER_TOKEN, {
     metaReducers
   }),
   StoreRouterConnectingModule,
-  LanguageModule,
+  CommonDeepstreamModule,
+  DeepstreamModule.forRoot(),
   GtrackCommonModule,
+  GenericComponentsModule,
+  AuthenticationApiModule.forRoot(getConfig),
+  LanguageModule,
+  DynamicModalModule,
   StoreDevtoolsModule.instrument({
     maxAge: 25
   }),
+  SearchFiltersModule.forRoot({
+    storeDomain: 'searchFilters'
+  }),
+  SharedModule.forRoot(sharedConfig),
+  BackgroundGeolocationModule.forRoot(),
+  EffectsModule.forRoot([HikeEffects, PoiEffects, RouteEffects, GeoSearchEffects, BackgroundGeolocationEffects]),
+  GeoSearchModule.forRoot({
+    storeDomain: 'geosearch'
+  }),
+  HikeModule.forRoot(hikeModuleConfig),
+  BackgroundGeolocationModule.forRoot(),
+  CommonAuthenticationModule,
   CoreLayoutModule,
   AuthModule,
   HikeListModule,
