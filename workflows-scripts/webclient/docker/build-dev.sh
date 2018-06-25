a98eb62b9535dea8ab7bbfd369df401ffe21c047
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-06-25 15:53:10 +0200

    git subrepo pull (merge) workflows-scripts
    subrepo:
      subdir:   "workflows-scripts"
      merged:   "ca14b7e"
    upstream:
      origin:   "https://github.com/garlictech/workflows-scripts.git"
      branch:   "master"
      commit:   "ca14b7e"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "3dad157"

--- workflows-scripts/webclient/docker/build-dev.sh
+++ workflows-scripts/webclient/docker/build-dev.sh
@@ -3,4 +3,4 @@ set -e
 . .env
 
 docker-compose -f docker/docker-compose.webpack.yml build $@
-docker-compose -f docker/docker-compose.webpack.yml run ${PROJECT}.webpack-server npm run build:dll
+docker-compose -f docker//docker-compose.webpack.yml run ${PROJECT}.webpack-server npm run build:dll
