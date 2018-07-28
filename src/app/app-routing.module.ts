3e6d97698df28f4bb37ae12fec7feb2bfb8f800c
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-07-28 13:30:04 +0200

    fix: a build problem fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Route } from '@angular/router';
 import { AuthGuard } from 'subrepos/authentication-api-ngx';
-import { PageNotFoundComponent } from './core/components/page-not-found';
+import { NotFound404Component } from './not-found404.component';
 import { LoginComponent } from './auth/components/login';
 import { HikeListComponent } from './pages/hike-list';
 import { HikeEditComponent } from './pages/hike-edit';
 import { LayoutComponent } from './core/components/layout';
+
+const fallbackRoute: Route = { path: '**', component: NotFound404Component };
 
 export const routes: Routes = [
   {
@@ -40,10 +42,7 @@ export const routes: Routes = [
     path: 'logout',
     component: LoginComponent
   },
-  {
-    path: '**',
-    component: PageNotFoundComponent
-  }
+  fallbackRoute
 ];
 
 @NgModule({
