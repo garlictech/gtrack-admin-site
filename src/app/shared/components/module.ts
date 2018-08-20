6b241a9eb82e71b479050d8f7ca83e693510e0df
Petró Tamás <petro.tamas@gmail.com>
2018-08-20 21:41:59 +0200

    feat: Marker onClick modal
    Close #288

--- src/app/shared/components/module.ts
+++ src/app/shared/components/module.ts
@@ -7,19 +7,35 @@ import { FormModule } from 'subrepos/gtrack-common-web/forms';
 import { LocalizedDescriptionComponent } from './localized-description';
 import { TableSpinnerComponent } from './table-spinner';
 import { AdminLeafletComponent } from './admin-leaflet';
+import { AdminMarkerPopupComponent } from './admin-marker-popup';
+import { LocalizeModule } from 'subrepos/gtrack-common-ngx';
 
+const COMPONENTS = [
+  AdminLeafletComponent,
+  LocalizedDescriptionComponent,
+  TableSpinnerComponent,
+  AdminMarkerPopupComponent
+]
 @NgModule({
   imports: [
     CommonModule,
     FormModule,
     FormsModule,
     FontAwesomeModule,
+    LocalizeModule,
     // PrimeNG
     DropdownModule,
     AccordionModule,
     ButtonModule
   ],
-  declarations: [AdminLeafletComponent, LocalizedDescriptionComponent, TableSpinnerComponent],
-  exports: [AdminLeafletComponent, LocalizedDescriptionComponent, TableSpinnerComponent]
+  declarations: [
+    ...COMPONENTS
+  ],
+  exports: [
+    ...COMPONENTS
+  ],
+  entryComponents: [
+    AdminMarkerPopupComponent
+  ]
 })
 export class SharedComponentsModule {}
