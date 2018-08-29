1f32443c18d5d51988b78b8a9d11863c89d3ef88
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-08-29 02:07:47 +0200

    git subrepo pull (merge) src/subrepos/gtrack-common-ngx
    subrepo:
      subdir:   "src/subrepos/gtrack-common-ngx"
      merged:   "194590c"
    upstream:
      origin:   "https://github.com/garlictech/gtrack-common-ngx.git"
      branch:   "master"
      commit:   "ddfbdb8"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "3dad157"

--- src/subrepos/gtrack-common-ngx/app/hike/components/search-filters/search-filters.component.ts
+++ src/subrepos/gtrack-common-ngx/app/hike/components/search-filters/search-filters.component.ts
@@ -3,6 +3,8 @@ import { Store } from '@ngrx/store';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Subject } from 'rxjs';
 
+import { SliderField, RangeSliderField, IFormDescriptor } from 'subrepos/forms-ngx';
+
 import * as filterActions from '../../../search-filters/store/actions';
 
 import { SearchFiltersSelectors } from '../../../search-filters/store';
@@ -12,6 +14,8 @@ import { SearchFiltersSelectors } from '../../../search-filters/store';
   template: ''
 })
 export class SearchFiltersComponent implements OnInit, OnDestroy {
+  public formDescriptor: IFormDescriptor;
+
   public filterForm: FormGroup;
   public showFilters = false;
 
@@ -33,6 +37,40 @@ export class SearchFiltersComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.formDescriptor = {
+      submit: { submitFv: (formGroup: FormGroup) => this.onChanges(formGroup.value) },
+      fields: {
+        radius: new SliderField({
+          label: 'common.hike.location-search.max-distance',
+          min: 1,
+          max: 200,
+          defaultValue: 50,
+          submitOnChange: true
+        }),
+        difficulty: new RangeSliderField({
+          label: 'common.hike.location-search.difficulty',
+          min: 0,
+          max: 10,
+          defaultValue: [3, 9],
+          submitOnChange: true
+        }),
+        length: new RangeSliderField({
+          label: 'common.hike.location-search.distance',
+          min: 1,
+          max: 50,
+          defaultValue: [5, 50],
+          submitOnChange: true
+        }),
+        time: new RangeSliderField({
+          label: 'common.hike.location-search.length',
+          min: 0,
+          max: 10,
+          defaultValue: [3, 10],
+          submitOnChange: true
+        })
+      }
+    };
+
     this.filterForm.valueChanges
       .debounceTime(300)
       .takeUntil(this._destroy$)
---
+++ src/subrepos/gtrack-common-ngx/app/object-mark/store/actions.ts
@@ -0,0 +1,39 @@
+import { Action } from '@ngrx/store';
+import { EObjectMarkContext } from 'subrepos/provider-client';
+
+export enum ObjectMarkActionTypes {
+  LOAD_CONTEXT = '[ObjectMark] Load context',
+  CONTEXT_LOADED = '[ObjectMark] Context loaded',
+  MARK_OBJECT = '[ObjectMark] Mark object',
+  OBJECT_MARKED = '[ObjectMark] Object marked'
+};
+
+export class LoadContext implements Action {
+  readonly type = ObjectMarkActionTypes.LOAD_CONTEXT;
+
+  constructor(public context: EObjectMarkContext) {}
+}
+
+export class ContextLoaded implements Action {
+  readonly type = ObjectMarkActionTypes.CONTEXT_LOADED;
+
+  constructor(public context: EObjectMarkContext, public objects: any[]) {}
+}
+
+export class MarkObject implements Action {
+  readonly type = ObjectMarkActionTypes.MARK_OBJECT;
+
+  constructor(public context: EObjectMarkContext, public object: any, public mark: boolean) {}
+}
+
+export class ObjectMarked implements Action {
+  readonly type = ObjectMarkActionTypes.OBJECT_MARKED;
+
+  constructor(public context: EObjectMarkContext, public object: any, public mark: boolean) {}
+}
+
+export type AllObjectMarkActionTypes =
+  | LoadContext
+  | ContextLoaded
+  | MarkObject
+  | ObjectMarked;
