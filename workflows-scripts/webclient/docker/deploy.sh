0cada3d48a7909795afa09ae7d9b36e6d00792f6
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-09-12 12:12:26 +0200

    fix: deploy script fix

--- workflows-scripts/webclient/docker/deploy.sh
+++ workflows-scripts/webclient/docker/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
+. .env
 
 docker run -i -t --rm \
   -v $(pwd):/app/project \
   -e FIREBASE_TOKEN="$FIREBASE_TOKEN" \
-  garlictech2/workflows-common:${npm_package_config_dockerWorkflowVersion} scripts/firebase-deploy.sh
+  garlictech2/workflows-common:${npm_package_config_dockerWorkflowVersion} scripts/firebase-deploy.sh $PROJECT_CONFIG
