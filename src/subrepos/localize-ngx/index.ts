1db1821777d966ed3f1ca006dce2ed52ba6662a9
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-09-18 21:46:32 +0200

    n/a

--- src/subrepos/localize-ngx/index.ts
+++
@@ -1,5 +0,0 @@
-export { LocalizeModule, defaultLocalizeConfig, ILocalizeConfig, CONFIG } from './lib';
-export { LanguageSelectorComponent } from './language-selector';
-export { ILanguageDesc } from './languages';
-export { Actions, Reducer, ILocalizationState } from './store';
-export { LanguageService } from './language-service';
