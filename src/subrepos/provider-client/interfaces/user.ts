014c61fa17e266ea3755f40a83fd782cd24d5ddb
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-09-18 19:26:20 +0200

    git subrepo pull src/subrepos/provider-client
    subrepo:
      subdir:   "src/subrepos/provider-client"
      merged:   "0db4c22"
    upstream:
      origin:   "https://github.com/garlictech/gtrack-provider-client.git"
      branch:   "master"
      commit:   "0db4c22"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "b623863"

--- src/subrepos/provider-client/interfaces/user.ts
+++ src/subrepos/provider-client/interfaces/user.ts
@@ -1,8 +1,5 @@
 import { IProviderInput } from './provider';
-import { EAuthRoles } from './roles';
-import { IPublicCommonProfile, ICommonProfile, ICommonProfileSettings, ICommonProfileData } from './profile';
-
-export interface IPublicUserProfile extends IPublicCommonProfile {}
+import { ICommonProfile, ICommonProfileSettings, ICommonProfileData } from './profile';
 
 export interface IUserProfileBasic extends ICommonProfile {
   birthDate?: string;
@@ -11,14 +8,12 @@ export interface IUserProfileBasic extends ICommonProfile {
   languages?: string[];
 }
 
-export interface IUserProfileSettings extends ICommonProfileSettings {}
-
 export interface IUserProfile {
   basic: IUserProfileBasic;
-  settings: IUserProfileSettings;
+  settings: ICommonProfileSettings;
 }
 
 export interface IUserData extends ICommonProfileData<IUserProfile> {}
 
 export interface IUserProfileBasicInput extends IUserProfileBasic, IProviderInput {}
-export interface IUserProfileSettingsInput extends IUserProfileSettings, IProviderInput {}
+export interface IUserProfileSettingsInput extends ICommonProfileSettings, IProviderInput {}
