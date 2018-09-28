6ab50a23fe501b7b88b93c7a3a8c65c7f0f4e397
Petró Tamás <petro.tamas@gmail.com>
2018-09-28 02:09:10 +0200

    feat: ETextualDescriptionType enum (markdown)

--- src/subrepos/provider-client/interfaces/common.ts
+++ src/subrepos/provider-client/interfaces/common.ts
@@ -1,7 +1,12 @@
+export enum ETextualDescriptionType {
+  markdown = 'markdown'
+}
+
 export interface ITextualDescription {
   title: string;
   summary?: string;
   fullDescription?: string;
+  type?: ETextualDescriptionType;
 }
 
 export interface ILocalizedItem<T> {
