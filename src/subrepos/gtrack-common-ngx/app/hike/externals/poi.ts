a8fc9227681714b6ef7935658e92c6fac0e0d190
Petró Tamás <petro.tamas@gmail.com>
2018-09-28 09:35:35 +0200

    subrepo update

--- src/subrepos/gtrack-common-ngx/app/hike/externals/poi.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export interface IExternalPoiDependencies {
-  storeDomain: string;
-}
-
-export const EXTERNAL_POI_DEPENDENCIES = new InjectionToken<IExternalPoiDependencies>('IExternalPoiDependencies');
--- src/subrepos/gtrack-common-ngx/app/hike/externals/route.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export interface IExternalRouteDependencies {
-  storeDomain: string;
-}
-
-export const EXTERNAL_ROUTE_DEPENDENCIES = new InjectionToken<IExternalRouteDependencies>('IExternalRouteDependencies');
