e2e9799cb67213f9dd1f2a5932c5ee759421f88f
Tibor Ujlaky <tujlaky@gmail.com>
2019-03-05 15:01:03 +0000

    fix: Rerelase the old lambdas
    Closes gtrack#24

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -7,10 +7,10 @@ export class Environment implements IEnvironment {
   // deepstream = 'wss://deepstream-staging.gtracksport.com/deepstream';
   deepstream = 'wss://deepstream.staging.gtracksport.com/deepstream';
 
-  lambdaEndpoint = 'https://y3nuwgmlpc.execute-api.us-east-1.amazonaws.com/latest';
+  lambdaEndpoint = 'https://njpebvpadh.execute-api.us-east-1.amazonaws.com/latest';
 
   authentication = {
-    server: 'https://lz679q1jla.execute-api.us-east-1.amazonaws.com/latest',
+    server: 'https://9i0oeair61.execute-api.us-east-1.amazonaws.com/latest',
     google: { appId: '941049973777-8pdbs3vi9veua8i21fbnhkmku74s00dm.apps.googleusercontent.com' }
   };
 
