2fc3d4ac0c3239332ae41d96c7548c4a032afb51
Tamás Petró <petro.tamas@gmail.com>
2019-03-29 00:56:29 +0100

    turn off marker icon feature store tests

---
+++ src/features/common/marker-icons/store/test/selectors.spec.ts
@@ -0,0 +1,24 @@
+/*
+import * as fromSelectors from '../selectors';
+import { createState } from './utils';
+
+
+const testCases = [
+  {
+    name: 'getMapId',
+    selector: fromSelectors.getMapId,
+    state: createState()
+  },
+  {
+    name: 'selectFeatureId',
+    selector: fromSelectors.selectFeatureId,
+    state: createState({ featureId: 2 })
+  }
+];
+
+testCases.forEach(({ name, selector, state }) => {
+  test(`${name} with input ${JSON.stringify(state)}`, () => {
+    expect(selector(state)).toMatchSnapshot();
+  });
+});
+*/
---
+++ src/features/common/marker-icons/store/test/utils.ts
@@ -0,0 +1,11 @@
+import { State } from '../state';
+/*
+export const createFeatureState = ({ markers, icons } = {}): State => ({
+  markers,
+  icons
+});
+
+export const createState = (stateParams?): { 'features.marker-icons': State } => ({
+  'features.marker-icons': createFeatureState(stateParams)
+});
+*/
